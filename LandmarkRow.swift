240ca7d7dc5044659927587a84fd57879c1b065d
Aleksey Kukushkin <Kuckushckin.alex@yandex.ru>
2021-03-18 22:40:51 +0300

    Customise the Row Preview #10

--- LandmarkRow.swift
+++ LandmarkRow.swift
@@ -9,21 +9,25 @@ import SwiftUI
 
 struct LandmarkRow: View {
     var landmark: Landmark
-    
+
     var body: some View {
         HStack {
             landmark.image
                 .resizable()
                 .frame(width: 50, height: 50)
             Text(landmark.name)
-            
-            Spacer();
+
+            Spacer()
         }
     }
 }
 
 struct LandmarkRow_Previews: PreviewProvider {
     static var previews: some View {
-        LandmarkRow(landmark: landmarks[0])
+        Group {
+            LandmarkRow(landmark: landmarks[0])
+            LandmarkRow(landmark: landmarks[1])
+        }
+        .previewLayout(.fixed(width: 300, height: 70))
     }
 }
