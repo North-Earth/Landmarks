59f6fd9fbc4aac04e136df5f913c2754718b08a6
Aleksey Kukushkin <Kuckushckin.alex@yandex.ru>
2021-03-21 23:15:00 +0300

    Mark the User’s Favourite Landmarks #12

--- LandmarkRow.swift
+++ LandmarkRow.swift
@@ -18,6 +18,11 @@ struct LandmarkRow: View {
             Text(landmark.name)
 
             Spacer()
+            
+            if landmark.isFavorite {
+                Image(systemName: "star.Fill")
+                    .foregroundColor(.yellow)
+            }
         }
     }
 }
