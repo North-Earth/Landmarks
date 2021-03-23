e39574b0402ec0c599415792750ce228569caa52
Aleksey Kukushkin <Kuckushckin.alex@yandex.ru>
2021-03-23 19:26:07 +0300

    Created Drawing Data for a Badge View #14

---
+++ Landmarks/Views/Helpers/BadgeBackground.swift
@@ -0,0 +1,20 @@
+//
+//  BadgeBackground.swift
+//  Landmarks
+//
+//  Created by Aleksey Kukushkin on 23.03.2021.
+//
+
+import SwiftUI
+
+struct BadgeBackground: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BadgeBackground_Previews: PreviewProvider {
+    static var previews: some View {
+        BadgeBackground()
+    }
+}
