b76c7cf59bb79f162a41805a917097f52a0587c5
Aleksey Kukushkin <Kuckushckin.alex@yandex.ru>
2021-03-26 23:43:45 +0300

    Added Animations to Individual Views #18

--- Landmarks/Views/Hikes/HikeView.swift
+++ Landmarks/Views/Hikes/HikeView.swift
@@ -25,6 +25,7 @@ struct HikeView: View {
                     Image(systemName: "chevron.right.circle")
                         .imageScale(.large)
                         .rotationEffect(.degrees(showDetail ? 90 : 0))
+                        .scaleEffect(showDetail ? 1.5 : 1)
                         .padding()
                 }
             }
