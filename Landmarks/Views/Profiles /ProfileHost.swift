45e76effaf5754083e26f3e074ec5fd17379b643
Aleksey Kukushkin <Kuckushckin.alex@yandex.ru>
2021-03-28 12:17:02 +0300

    Display a User Profile #22

---
+++ Landmarks/Views/Profiles /ProfileHost.swift
@@ -0,0 +1,25 @@
+//
+//  ProfileHost.swift
+//  Landmarks
+//
+//  Created by Aleksey Kukushkin on 28.03.2021.
+//
+
+import SwiftUI
+
+struct ProfileHost: View {
+    @State private var draftProfile = Profile.default
+
+    var body: some View {
+        VStack(alignment: .leading, spacing: 20) {
+            ProfileSummary(profile: draftProfile)
+        }
+        .padding()
+    }
+}
+
+struct ProfileHost_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileHost()
+    }
+}
