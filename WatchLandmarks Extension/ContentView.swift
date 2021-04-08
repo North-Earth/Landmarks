dfa557b5787e3911efb3646b95af7b122eef04b5
Aleksey Kukushkin <Kuckushckin.alex@yandex.ru>
2021-04-08 21:37:19 +0300

    Added a watchOS Target #27

---
+++ WatchLandmarks Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WatchLandmarks Extension
+//
+//  Created by Aleksey Kukushkin on 08.04.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
